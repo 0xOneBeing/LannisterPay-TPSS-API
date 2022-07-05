d7ae98ea56c6e77d71527ed2ddfb65155a45c270
Sunday Obinna Oruwhone <sundayamosobi@gmail.com>
2022-07-05 07:06:34 +0100

    Routes for LannisterPay-TPSS

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const routes = require('./routes')
+
+module.export = function(app) {
+    routes(app)
+}
---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+module.export = function(app) {
+    let id, amount, currency, customerEmail;
+    app.post('./split-payments/compute', (res, req) => {
+        // res.send(ID, Amount, Currency, CustomerEmail)
+        function createSplit() {
+            
+        }
+    })
+
+
+}
